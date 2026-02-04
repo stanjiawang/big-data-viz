d2121c86b8c1893925b5b9186a88d689538f66fe
Stan Wang <stan@Stans-MacBook-Pro.local>
2026-02-03 16:36:27 -0800

    Fix lint disable for app mode

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="vite/client" />
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare const __APP_MODE__: string | undefined;
